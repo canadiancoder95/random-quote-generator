5754d790c145de583e95134586c01865bb6fb2dd
Noah Malchy <50896885+canadiancoder95@users.noreply.github.com>
2020-01-08 13:52:22 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  function getNewQuote() {
+    $.ajax({ 
+    type : "GET", 
+    jsonp: 'jsonp',
+    dataType: 'jsonp',
+    url : "https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en",
+    success : function(result) { 
+      if (result.quoteAuthor.length > 1) {
+        $('h1').html(' "' + result.quoteText + '" ' + " - " + result.quoteAuthor);
+      } else {
+        $('h1').html(' " ' + result.quoteText + ' " ' + " - " + "Unknown")
+      }
+    },
+    error : function(result) { 
+       console.log("Error, figure it out idiot.")
+      }
+    });
+   }
+var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16)
+$('#mybutton').on('click', function() {
+  getNewQuote()
+  randomColor = "#" + Math.floor(Math.random()*16777215).toString(16)
+  });
+$('#mybutton').on('click', function() {
+  $(this).animate({"background-color": randomColor}, 1000)
+  });
+$('#quote-box').on('click', function() {
+  $(this).animate({"color": randomColor}, 1000)
+  });
+$('body').on('click', function() { 
+  $(this).animate({"background-color": randomColor}, 1000)
+  });
+});
